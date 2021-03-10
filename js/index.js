26f5bdaedfb115d1a38e98c0021412dcb949c3d4
rhammer35 <rhammer35@icloud.com>
2021-03-10 09:28:27 -0500

    added first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// Define variables for selected elements
+const nav = document.querySelector(".nav");
+const bus = document.querySelector(".intro, img");
+const btn = document.querySelector(".btn");
+
+// Add mouseover event listener
+nav.addEventListener("mouseover", function (event) {
+    event.target.style.color = "#fad6a5";
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+}, false);
+
+
+
